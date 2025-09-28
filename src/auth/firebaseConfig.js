f5cc26abdbf142e76fba6fbfc6ffbab808d5193c
gowrish <gowrishspatel@gmail.com>
2025-09-28 16:07:27 +0530

    update .env file

--- src/auth/firebaseConfig.js
+++ src/auth/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5w1gkPTy9EcFC_rXXtaOzojC7d4-zTIA",
-  authDomain: "library-management-14955.firebaseapp.com",
-  projectId: "library-management-14955",
-  storageBucket: "library-management-14955.firebasestorage.app",
-  messagingSenderId: "564740030401",
-  appId: "1:564740030401:web:a2410bf22e6b2242513940",
-  measurementId: "G-EDQJB1YWXJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
